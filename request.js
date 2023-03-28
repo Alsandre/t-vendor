715a96998b4a5aeae27bc05b1da437a22906d642
Aleksandre <gabeditebuli@gmail.com>
2023-03-28 18:40:44 +0400

    fix: webpack config

--- request.js
+++ request.js
@@ -2,9 +2,11 @@ import { mainContentRender, tagListRender } from "./utility.js";
 
 export const requestData = async (tagList) => {
   const res = await fetch(
-    "https://tvendor-4db67-default-rtdb.europe-west1.firebasedatabase.app/productList.json"
+    "https://tvendor-4db67-default-rtdb.europe-west1.firebasedatabase.app/productList.json", //this endpoint have some issues
   );
+  
   const data = await res.json();
+  console.log(JSON.stringify(data))
   data.forEach((element) => {
     element.categoryTag.forEach((tagname) => {
       if (!tagList.includes(tagname)) {
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import { $, tagListRender } from "../utility.js";
+import {
+  basketClickHandler,
+  clearBasketHandler,
+  clearTagHandler,
+  onBasketClose,
+  searchForNameHandler,
+  searchForTagHandler,
+} from "../event-handlers.js";
+import { requestData } from "../request.js";
+
+export let uniqueTagNames = [];
+
+const basketBtn = $(".basket-button");
+const backdrop = $("#backdrop");
+const basketClearBtn = $("#clear-basket");
+const basketCloseBtn = $("#close-basket");
+const tagSearchBar = $("#search-bar");
+const nameSearchBar = $("#search-by-name");
+const clearTagnameBtn = $(".clear-tagname");
+
+nameSearchBar.oninput = (event) => searchForNameHandler(event);
+
+clearTagnameBtn.onclick = clearTagHandler;
+
+tagSearchBar.oninput = (e) =>
+  searchForTagHandler(e, uniqueTagNames, tagListRender);
+
+basketCloseBtn.onclick = onBasketClose;
+
+basketClearBtn.onclick = clearBasketHandler;
+
+backdrop.onclick = onBasketClose;
+
+basketBtn.onclick = basketClickHandler;
+
+requestData(uniqueTagNames);
+
